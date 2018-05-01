723c9768f9b6603b7961b24cb72d952090f010c3
Jaha <jaha.makwana@heliossolutions.in>
2018-05-01 18:44:41 +0530

    remove slow mode banner.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Database Example',
       home: new MyHomePage(),
     );
